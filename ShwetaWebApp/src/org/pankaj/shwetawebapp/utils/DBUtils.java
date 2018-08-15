52a517fd6198192d90b98d040a3e5c87715c32fe
Shweta Singh <intelshweta@gmail.com>
2018-08-14 23:12:19 -0500

    utilities method

---
+++ ShwetaWebApp/src/org/pankaj/shwetawebapp/utils/DBUtils.java
@@ -0,0 +1,127 @@
+package org.pankaj.shwetawebapp.utils;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.pankaj.shwetawebapp.Product;
+import org.pankaj.shwetawebapp.User_Acct;
+
+public class DBUtils {
+	public static User_Acct findUser(Connection conn,
+			String userName,String password)throws SQLException{
+		
+		String sql = "Select User_Name,Password,Gender from User_Acct where User_Name =? and password=?";
+		
+		PreparedStatement pstm = conn.prepareStatement(sql);
+		pstm.setString(1, userName);
+		pstm.setString(2, password);
+		 
+		ResultSet rs = pstm.executeQuery();
+		
+		if(rs.next()) {
+			String gender = rs.getString("Gender");
+			User_Acct user = new User_Acct();
+			user.setUserName(userName);
+			user.setPassword(password);
+			user.setGenderName(gender);
+		
+			return user;
+		}
+		return null;
+		
+	}
+	public static User_Acct findUser(Connection conn,String userName)throws SQLException{
+		
+		String sql = "Select User_Name,Password,Gender from User_Acct where User_Name =?";
+		PreparedStatement pstm = conn.prepareStatement(sql);
+		pstm.setString(1, userName);
+		
+		ResultSet rs= pstm.executeQuery();
+		
+		if(rs.next()) {
+			String password = rs.getString("Password");
+			String gender = rs.getString("Gender");
+			User_Acct user = new User_Acct();
+			user.setUserName(userName);
+			user.setPassword(password);
+			user.setGenderName(gender);
+			return user;
+		}
+		return null;
+		
+	}
+public static List<Product>queryProduct(Connection conn)throws SQLException{
+	String sql = "Select Code,Name,Price from Product";
+	
+	PreparedStatement pstm = conn.prepareStatement(sql);
+	
+	ResultSet rs= pstm.executeQuery();
+	List<Product>list= new ArrayList<Product>();
+	while(rs.next()) {
+		String code = rs.getString("Code");
+		String name = rs.getString("Name");
+		float price =  rs.getFloat("Price");
+		Product product = new Product();
+		product.setCode(code);
+		product.setName(name);
+		product.setPrice(price);
+		list.add(product);
+		
+	}
+	return list;
+	
+}
+	public static Product findProduct(Connection conn,String code)throws SQLException{
+		String sql = "Select Code,Name,Price from Product where Code=?";
+		
+		
+		PreparedStatement pstm = conn.prepareStatement(sql);
+	pstm.setString(1, code);
+	ResultSet rs= pstm.executeQuery();
+	
+	while(rs.next()) {
+		String name = rs.getString("Name");
+		float price =  rs.getFloat("Price");
+		Product product = new Product(code,name,price);
+		return product;
+	
+	}
+		return null;
+	}
+	public static void updateProduct(Connection conn,Product product)throws SQLException{
+		String sql = "Update Product set Name=?,Price=? where Code=?";
+		
+		
+		PreparedStatement pstm = conn.prepareStatement(sql);
+	pstm.setString(1, product.getName());
+	pstm.setFloat(2,product.getPrice());
+	pstm.setString(3,product.getCode());
+	pstm.executeUpdate();
+	
+	}
+	public static void insertProduct(Connection conn,Product product)throws SQLException{
+		String sql = "Insert into Product(Code,Name,Price)values(???)";
+		
+		PreparedStatement pstm = conn.prepareStatement(sql);
+	
+		pstm.setString(1, product.getCode());
+	pstm.setString(2,product.getName());
+	pstm.setFloat(3,product.getPrice());
+	pstm.executeUpdate();
+	}
+	
+public static void deleteProduct(Connection conn,String code)throws SQLException{
+	String sql = "Delete from Product where Code=?";
+	
+	
+	PreparedStatement pstm = conn.prepareStatement(sql);
+	
+	pstm.setString(1,code);
+	pstm.executeUpdate();
+	
+}
+}
---
+++ ShwetaWebApp/src/org/pankaj/shwetawebapp/utils/MyUtils.java
@@ -0,0 +1,60 @@
+package org.pankaj.shwetawebapp.utils;
+
+import java.sql.Connection;
+
+import javax.servlet.ServletRequest;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.pankaj.shwetawebapp.User_Acct;
+
+public class MyUtils {
+
+	public static final String ATT_NAME_CONNECTION = "ATTRIBUTE_FOR_CONNECTION";
+	public static final String ATT_NAME_USER_NAME = "ATTRIBUTE_FOR_STORE_USER_NAME_IN_COOKIE";
+	
+	
+	public static void StoreConnevtion(ServletRequest request,Connection conn) {
+		request.setAttribute(ATT_NAME_CONNECTION, conn);
+		
+	}
+	public static Connection getStoredConnection(ServletRequest request) {
+		Connection conn =(Connection)request.getAttribute(ATT_NAME_CONNECTION);
+		return conn;
+				
+	}
+	public static void storeLoginedUser(HttpSession session,User_Acct loginedUSer) {
+		session.setAttribute("loginedUser", loginedUSer);
+	}
+	public static User_Acct getLoginedUser(HttpSession session) {
+		User_Acct loginedUser = (User_Acct)session.getAttribute("loginedUser");
+		return loginedUser;
+		
+	}
+	public static void storeUserCookie(HttpServletResponse response,User_Acct user) {
+		System.out.println("Store user cookie");
+		Cookie cookieUserName =new Cookie(ATT_NAME_USER_NAME,user.getUserName());
+		cookieUserName.setMaxAge(24*60*60);
+		response.addCookie(cookieUserName);
+	}
+	public static String getUserNamelnCookie(HttpServletRequest request) {
+		Cookie[] cookies=request.getCookies();
+		if(cookies!=null) {
+			for(Cookie cookie :cookies) {
+				if(ATT_NAME_USER_NAME.equals(cookie.getName())) {
+					
+				}
+			}
+		}
+			return null;
+			
+	}
+	public static void deleteUserCookie(HttpServletResponse response) {
+		Cookie cookieUserName=new Cookie(ATT_NAME_USER_NAME,null);
+		cookieUserName.setMaxAge(0);
+		response.addCookie(cookieUserName);
+		
+	}
+}
